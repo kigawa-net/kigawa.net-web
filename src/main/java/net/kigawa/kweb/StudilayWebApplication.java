fc9deca1780d364babcecd0ac3e48c109ab61ccf
kigawa <kigawa.8390@gmail.com>
2022-08-01 22:37:30 +0900

    add root

--- src/main/java/net/kigawa/kweb/StudilayWebApplication.java
+++ src/main/java/net/kigawa/kweb/StudilayWebApplication.java
@@ -1,10 +1,7 @@
 package net.kigawa.kweb;
 
-import net.kigawa.kweb.response.HelloResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
@@ -18,11 +15,5 @@ public class StudilayWebApplication
     public static void main(String[] args)
     {
         SpringApplication.run(StudilayWebApplication.class, args);
-    }
-
-    @GetMapping("/hello")
-    public HelloResponse hello(@RequestParam(value = "name", defaultValue = "World") String name)
-    {
-        return new HelloResponse(atomicLong.incrementAndGet(), String.format("Hello %s", name));
     }
 }
--- src/main/java/net/kigawa/kweb/response/HelloResponse.java
+++
@@ -1,5 +0,0 @@
-package net.kigawa.kweb.response;
-
-public record HelloResponse(long id, String content)
-{
-}
